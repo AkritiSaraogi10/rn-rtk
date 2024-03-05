9f3426eb9fc7abf0ba06b356a592fbe23d2468fd
AkritiSaraogi <akritisaraogi@gmail.com>
2024-03-05 16:42:10 +0530

    modal

--- components/customModal.tsx
+++ components/customModal.tsx
@@ -1,38 +1,70 @@
 import React, {useState} from 'react';
-import {Modal, Text, View, Button} from 'react-native';
+import {Text, View, TouchableOpacity, StyleSheet} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {setErrorToast} from '../reducers/globalSlice';
+import {Portal, Modal, PaperProvider} from 'react-native-paper';
 
-interface CustomModalProps {
-  message: string;
-  description: string;
-  onClose: () => void;
-}
+// interface CustomModalProps {
+//   message: string;
+//   description: string;
+//   onClose: () => void;
+// }
 
-const CustomModal: React.FC<CustomModalProps> = ({
-  message,
-  description,
-  onClose,
-}) => {
+const CustomModal: React.FC = ({}) => {
   const [isVisible, setIsVisible] = useState(true);
+  const error = useSelector((state: any) => state.global.error);
+  const dispatch = useDispatch();
 
   const closeModal = () => {
-    setIsVisible(false);
-    onClose();
+    dispatch(setErrorToast({status: false, message: ''}));
+    // setIsVisible(false);
+    // onClose();
   };
+  console.log('custom---> ', error);
+
+  const styles = StyleSheet.create({
+    button: {
+      backgroundColor: '#7C9B32',
+      padding: 10,
+      borderRadius: 20,
+      width: 70,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    buttonText: {
+      color: '#fff',
+    },
+    buttonContainer: {
+      marginTop: 40,
+      alignItems: 'center',
+    },
+  });
 
   return (
-    <Modal
-      transparent={true}
-      animationType="slide"
-      visible={isVisible}
-      onRequestClose={closeModal}>
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <View style={{backgroundColor: 'white', padding: 20, borderRadius: 10}}>
-          <Text>{message}</Text>
-          <Text>{description}</Text>
-          <Button title="Close" onPress={closeModal} />
+    // <PaperProvider>
+    <Portal>
+      <Modal visible={error.status}>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <View
+            style={{
+              backgroundColor: '#DEE6C5',
+              padding: 20,
+              borderRadius: 10,
+              height: 250,
+              width: 250,
+            }}>
+            <Text style={{color: 'black'}}>{error.messageText}</Text>
+            <Text>{error.code}</Text>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity style={styles.button} onPress={closeModal}>
+                <Text style={styles.buttonText}>Ok</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
-      </View>
-    </Modal>
+      </Modal>
+    </Portal>
+    // </PaperProvider>
   );
 };
 
